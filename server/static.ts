ec34f59b8ea6d337a24ce5ef4b7e3ab909e8dce7
cryptocreeper94 <jason@livfitoken.com>
2026-04-12 13:15:08 -0500

    fix: use correct Express 5 wildcard syntax {*splat} instead of :path(*)

--- server/static.ts
+++ server/static.ts
@@ -26,7 +26,7 @@ export function serveStatic(app: Express) {
   });
 
   // fall through to index.html if the file doesn't exist
-  app.use("/:path(*)", (_req, res) => {
+  app.use("/{*splat}", (_req, res) => {
     res.sendFile(path.resolve(distPath, "index.html"));
   });
 }
